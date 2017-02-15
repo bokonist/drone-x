bf6dd199d6042104dc0525e6f2feee759db20023
Dani John <rocksdanister@users.noreply.github.com>
2017-02-15 20:50:13 +0530

    graphics test build (#2)

---
+++ graphics_test/myheader.h
@@ -0,0 +1,8 @@
+extern double queuedMilliseconds,prev0,fps,responseTime;
+extern int movementY,movementX;
+
+void init();
+void processSpecialKeys(int , int , int );
+void mixedStepLoop();
+void draw();
+void update();
