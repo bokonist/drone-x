181bba2f389753b7dbe319eec6c25e5432505a82
Dani John <rocksdanister@users.noreply.github.com>
2017-02-16 22:27:31 +0530

    Update draw.cpp (#3)

--- simulation/drone-window/draw.cpp
+++ simulation/drone-window/draw.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int width, height;
-static GLuint tex1;
+static GLuint texName[3];
 int movementY,movementX;
 unsigned char* image;
 
@@ -24,50 +24,57 @@ vector<obstacle> obstacleList;
 
 void init()
 {
-	glShadeModel(GL_FLAT);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+glShadeModel(GL_FLAT);
+glEnable(GL_BLEND);
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		
+	glGenTextures(3,texName); // 3 textures generated, identified by numbers stored in texName
+	
+        glBindTexture(GL_TEXTURE_2D, texName[0]);  //texture 1 active 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-}
 
-void texSelect(int textureNumber)
-{    
-	if(textureNumber==SPACE)
-	{
-		//glBindTexture(GL_TEXTURE_2D, tex1);
 		image = SOIL_load_image( "res/terrains/space-terrain.png", &width, &height, 0, SOIL_LOAD_RGBA );
 		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
 		SOIL_free_image_data( image );
-	}
-	if(textureNumber==DRONE)
-	{
+	
+ 	glBindTexture(GL_TEXTURE_2D, texName[1]);	
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	
 		image = SOIL_load_image( "res/drones/black-drone.png", &width, &height, 0, SOIL_LOAD_RGBA );
 		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
 		SOIL_free_image_data( image );
-	}
-	if(textureNumber==CLOUD)
-	{
+	
+	glBindTexture(GL_TEXTURE_2D, texName[2]);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
 		image = SOIL_load_image( "res/obstacles/cloud.png", &width, &height, 0, SOIL_LOAD_RGBA );
 		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
 		SOIL_free_image_data( image );
-	}  
+	
+glEnable(GL_TEXTURE_2D);
+	 
 }
 
-int objX,objY;
+int objX;
 int worldX;
 int angleRotate;
 
 void update()
 {
-	objX-=5;
-	objY--; 
+	objX-=5; // no need, use variable in structure
 	worldX-=8;
 
-	if(objX==-2700)
-		objX=0;
+	if(objX==-2700) // no need
+		objX=0; // no need
 
 	if(worldX<-2000)
 		worldX=0;
@@ -80,7 +87,7 @@ void update()
 
 void drawDrone()
 {
-	texSelect(DRONE);
+glBindTexture(GL_TEXTURE_2D, texName[1]); // drone texture active
 	glPushMatrix();
 		glTranslatef(40,500,0);
 		glTranslatef(movementX,movementY,0);
@@ -96,7 +103,7 @@ void drawDrone()
 
 void drawObstacle(int posx,int posy, int type)
 {
-	texSelect(type);
+glBindTexture(GL_TEXTURE_2D, texName[2]);
 	glPushMatrix();
 		glTranslatef(posx,posy,0);
 		glTranslatef(objX,0,0);
@@ -113,6 +120,8 @@ void draw()
 {
 	glClearColor (0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	/*
+	//top score bar, will check it later
 	glBegin(GL_QUADS);
 		// glColor3f(0.5,0.5,0.5);
 		glVertex3f(0,1080,-1);
@@ -120,12 +129,8 @@ void draw()
 		glVertex3f(1920,1040,-1);
 		glVertex3f(0,1040,-1);
 	glEnd();
-
-
-	glEnable(GL_TEXTURE_2D);
-	// glBindTexture(GL_TEXTURE_2D, tex1);
-	//glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-	texSelect(SPACE);
+	*/
+	glBindTexture(GL_TEXTURE_2D, texName[0]); // world texture active
 	glPushMatrix();
 		glTranslatef(worldX,0,0);
 		glBegin(GL_QUADS);
@@ -135,6 +140,7 @@ void draw()
 			glTexCoord2f(0.0, -1.0); glVertex3f(0,1080,-1.1);
 		glEnd();
 	glPopMatrix();
+	
 	drawDrone();
 
 	for(obstacle &obj : obstacleList)
@@ -145,7 +151,7 @@ void draw()
 	{
 		temp=0;
 		int tx= 2000+(rand()%600);
-		int ty= rand()%800;
+		int ty= rand()%950;
 		obstacle obj;
 		obj.x=tx;
 		obj.y=ty;
@@ -155,5 +161,5 @@ void draw()
 	else
 		temp++;
 	glutSwapBuffers();
-	glDisable(GL_TEXTURE_2D);
+
 }
