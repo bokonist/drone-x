913609400c9ee4219a3839df3010cbea80101a67
bokonist <jayrm1995@gmail.com>
2017-02-17 08:47:42 +0530

    repo restructure

--- simulation/drone-window/draw.cpp
+++ simulation/drone-window/draw.cpp
@@ -38,7 +38,7 @@ void init()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	image = SOIL_load_image( "res/terrains/space-terrain.png", &width, &height, 0, SOIL_LOAD_RGBA );
+	image = SOIL_load_image( "res/terrains/space-terrain-extended.png", &width, &height, 0, SOIL_LOAD_RGBA );
 	glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image );
 	SOIL_free_image_data( image );
 
