5a5fb7aa060b239a5d12e641e100f1c54a30152d
Dani John <rocksdanister@users.noreply.github.com>
2017-03-26 07:49:18 +0530

    Update drone.h

--- simulation/drone-v2/drone.h
+++ simulation/drone-v2/drone.h
@@ -2,7 +2,7 @@ extern double queuedMilliseconds,prev0,fps,responseTime,score;
 extern int movementY,movementX,speed,dronePhy;
 extern int resX,resY;
 
-void init();
+void initTextures();
 void processSpecialKeys(int , int , int );
 void mixedStepLoop();
 void draw();
