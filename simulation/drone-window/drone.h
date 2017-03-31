662f08cf25afbdb99ba006ebeccd0bac2d25a148
bokonist <jayrm1995@gmail.com>
2017-04-01 04:16:56 +0530

    delete obj files

--- simulation/drone-window/drone.h
+++
@@ -1,9 +0,0 @@
-extern double queuedMilliseconds,prev0,fps,responseTime,score;
-extern int movementY,movementX,speed,dronePhy;
-extern int resX,resY;
-void initTextures();
-void processSpecialKeys(int , int , int );
-void mixedStepLoop();
-void draw();
-void update();
-void movePhysics();
